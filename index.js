ac989d37a7b1f0db5a4091bcbc327ff964ca8cda
tyrell <tyrellancelogan@gmail.com>
2022-04-13 08:12:36 +0100

    basic app setup

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ const app = express();
 app.set("view engine", "ejs");
 // set views directory
 app.set("views", join(__dirname, "views"));
+// serve static files used by views
+app.use(express.static(join(__dirname, "public")));
 
 // *** MIDDLEWARE ***
 // parse url-encoded [form data]
 app.use(express.urlencoded({ extended: true }));
 // override post req to enable certain http verb req
+app.use(methodOverride("_method"));
+
+// *** ROUTING ***
 
 // start server and listen on port
+const portNumber = 8080;
+app.listen(portNumber, () => {
+    console.log(`listening on port ${portNumber}`);
+});
